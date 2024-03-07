bb10f8601700dfb21ca2e1577fb7fd5809ed3ce9
BhabhaSeptember <septemberbhabha@gmail.com>
2024-03-07 11:11:52 +0200

    commit

--- src/lesson11/Lesson11.java
+++ src/lesson11/Lesson11.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lesson10;
+package lesson11;
 
 /**
  *
  * @author User
  */
-public class Lesson10 {
-    public static void main(String[] args) {
-        System.out.println("");
-    }
-       
+public class Lesson11 {
+    
 }
